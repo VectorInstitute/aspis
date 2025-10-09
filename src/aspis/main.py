38c60d4e758475b065091b49585f71884964c570
Marcelo Lotif <marcelo.lotif@vectorinstitute.ai>
2025-10-09 13:40:40 -0400

    Adding the first 3 questions (product, risk and api key)

--- src/aspis/main.py
+++ src/aspis/main.py
@@ -12,54 +12,67 @@ def main() -> None:
 
     openai_api_key = st.session_state.get("openai_api_key", "")
     risk_description = st.session_state.get("risk_description", "")
+    product_description = st.session_state.get("product_description", "")
 
-    if not openai_api_key:
+    if not openai_api_key or not product_description or not risk_description:
         st.markdown("Welcome to Aspis!")
-        render_api_key_input()
+        with st.form("input_form"):
+            # Product description text area
+            current_product_description = st.text_area(
+                label="What is the description of your AI-powered product?",
+                placeholder="Enter your product description here...",
+                help=(
+                    "Your product description is used to generate a measurement instrument for an AI risk."
+                    + "Please describe your product in a comprehensive way."
+                ),
+            )
 
-    elif not risk_description:
-        render_risk_description_input()
+            # Risk description text area
+            current_risk_description = st.text_area(
+                label="What is the AI risk you want to create a measurement instrument for?",
+                placeholder="Enter your risk description here...",
+                help=(
+                    "Your risk description is used to generate a risk assessment. Please describe the"
+                    + "AI risk your product is exposed to in order to generate a measurement instrument."
+                ),
+            )
+
+            # API key text input
+            current_openai_api_key = st.text_input(
+                label="Enter your Open AI API key:",
+                placeholder="Paste your API key here...",
+                help="Your API key is used to authenticate your requests to the Open AI API.",
+                type="password",
+            )
+
+            if st.form_submit_button("Generate Questions", type="primary"):
+                if current_product_description.strip():
+                    st.session_state.product_description = current_product_description
+                else:
+                    st.error("Please enter a product description before proceeding.")
+                    return
+
+                if current_risk_description.strip():
+                    st.session_state.risk_description = current_risk_description
+                else:
+                    st.error("Please enter a risk description before proceeding.")
+                    return
+
+                if current_openai_api_key.strip():
+                    st.session_state.openai_api_key = current_openai_api_key
+                else:
+                    st.error("Please enter an Open AI API key before proceeding.")
+                    return
+
+                if (
+                    current_openai_api_key.strip()
+                    and current_product_description.strip()
+                    and current_risk_description.strip()
+                ):
+                    st.rerun()
 
     else:
         render_chat_ui(openai_api_key=openai_api_key, risk_description=risk_description)
-
-
-def render_api_key_input() -> None:
-    """Render the API key input form."""
-    with st.form("api_key_form"):
-        current_openai_api_key = st.text_input(
-            label="Enter your Open AI API key:",
-            placeholder="Paste your API key here...",
-            help="Your API key is used to authenticate your requests to the Open AI API.",
-            type="password",
-        )
-
-        if st.form_submit_button("Next", type="primary"):
-            if current_openai_api_key.strip():
-                st.session_state.openai_api_key = current_openai_api_key
-                st.rerun()
-            else:
-                st.error("Please enter an Open AI API key before proceeding.")
-
-
-def render_risk_description_input() -> None:
-    """Render the risk description input form."""
-    with st.form("risk_description_form"):
-        current_risk_description = st.text_area(
-            label="What is the AI risk you want to create a measurement instrument for?",
-            placeholder="Enter your risk description here...",
-            help=(
-                "Your risk description is used to generate a risk assessment. Please describe the"
-                + "AI risk your application is exposed to in order to generate a measurement instrument."
-            ),
-        )
-
-        if st.form_submit_button("Next", type="primary"):
-            if current_risk_description.strip():
-                st.session_state.risk_description = current_risk_description
-                st.rerun()
-            else:
-                st.error("Please enter a risk description before proceeding.")
 
 
 if __name__ == "__main__":
--- tests/aspis/test_main.py
+++ tests/aspis/test_main.py
@@ -16,8 +16,11 @@ def cleanup_session_state():
     st.session_state.clear()
 
 
+@patch("aspis.main.st.text_area")
 @patch("aspis.main.st.text_input")
-def test_main_render_api_key_input_when_empty(mock_text_input: Mock) -> None:
+def test_main_render_inputs_when_empty(
+    mock_text_input: Mock, mock_text_area: Mock
+) -> None:
     main()
 
     assert mock_text_input.call_count == 1
@@ -25,27 +28,24 @@ def test_main_render_api_key_input_when_empty(mock_text_input: Mock) -> None:
         mock_text_input.call_args_list[0].kwargs["label"]
         == "Enter your Open AI API key:"
     )
-
-
-@patch("aspis.main.st.text_area")
-def test_main_render_risk_input_when_api_key_is_set(mock_text_area: Mock) -> None:
-    st.session_state.openai_api_key = "test api key"
-
-    main()
-
-    assert mock_text_area.call_count == 1
+    assert mock_text_area.call_count == 2
     assert (
         mock_text_area.call_args_list[0].kwargs["label"]
+        == "What is the description of your AI-powered product?"
+    )
+    assert (
+        mock_text_area.call_args_list[1].kwargs["label"]
         == "What is the AI risk you want to create a measurement instrument for?"
     )
 
 
 @patch("aspis.main.render_chat_ui")
-def test_main_render_chat_ui_when_api_key_and_risk_description_are_set(
+def test_main_render_chat_ui_when_inputs_are_set(
     mock_render_chat_ui: Mock,
 ) -> None:
     st.session_state.openai_api_key = "test api key"
     st.session_state.risk_description = "test risk description"
+    st.session_state.product_description = "test product description"
 
     main()
 
@@ -54,3 +54,6 @@ def test_main_render_chat_ui_when_api_key_and_risk_description_are_set(
         mock_render_chat_ui.call_args_list[0].kwargs["risk_description"]
         == "test risk description"
     )
+
+
+# TODO test error messages
