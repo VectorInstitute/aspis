b06e533afb320a4332d021ccdd77d2ab190a6896
Marcelo Lotif <marcelo.lotif@vectorinstitute.ai>
2026-04-06 11:29:37 -0400

    Fixes to nginx and streamlit run to make it work on hf spaces

--- docker_services.sh
+++ docker_services.sh
@@ -4,8 +4,12 @@ set -e
 # Start FastAPI in background
 fastapi dev src/aspis/api/main.py --host 0.0.0.0 --port 8000 &
 
-# Start Streamlit in background
-streamlit run src/aspis/ui/main.py --server.address=0.0.0.0 --server.port=8501 &
+streamlit run src/aspis/ui/main.py \
+  --server.address=0.0.0.0 \
+  --server.port=8501 \
+  --server.enableCORS=false \
+  --server.enableXsrfProtection=false \
+  --server.headless=true &
 
 # Finally, run nginx in the foreground (PID 1)
 exec nginx -g "daemon off;"
